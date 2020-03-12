6ec07fdbbbbd27619e2aa4d0a4ce2c01aec2a829
Toxic254 <cyrus_g@outlook.com>
2020-03-12 11:21:54 +0300

    Made it a web application. With no node modules. Unable to run it on Windows, trying on Linux.

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const video = document.getElementById('video');
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
 function startVideo() {
-    navigator.getUserMedia(
+    navigator.mediaDevices.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
         err => console.err(err)
     )
 };
+
+video.addEventListener('play', () => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        console.log(detections);
+    }, 100);
+});
